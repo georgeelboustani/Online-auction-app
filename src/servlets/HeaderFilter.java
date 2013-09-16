efce7cf499c97872d895264c74934d83b6ba0aa0
Ryan Tan <ryantanjunming@gmail.com>
2013-09-17 03:44:01 +1000

    created HeaderFilter to handle login/logout sessions

---
+++ src/servlets/HeaderFilter.java
@@ -0,0 +1,72 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class HeaderFilter
+ */
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD
+		}
+					, urlPatterns = { "*.jsp" })
+public class HeaderFilter implements Filter {
+	
+	final Logger logger = Logger.getLogger(this.getClass().getName());
+	
+    /**
+     * Default constructor. 
+     */
+    public HeaderFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// pass the request along the filter chain
+		logger.info("HeaderFilter invoked");
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		//where we can check for session login..
+		HttpSession session = req.getSession(true);
+		if(session.getAttribute("user") == null){
+			res.sendRedirect("Landing/");
+		}else{
+			chain.doFilter(request, response);
+			res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate"); 
+			res.setHeader("Pragma", "no-cache"); 
+			res.setDateHeader("Expires", 0);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
